5ae441d32a4676a9e95ad26ba007b8b9e6813b79
24005-admin <yang.zhang.2000@gmail.com>
2024-10-30 22:59:17 +0800

    added itd ViperSlideByEncoder_test.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itd/ViperSlideByEncoder_test.java
@@ -0,0 +1,70 @@
+package org.firstinspires.ftc.teamcode.itd;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class ViperSlideByEncoder_test extends LinearOpMode {
+    DcMotor frontViper;
+    DcMotor backViper;
+    DcMotor testEncoder;
+    double speed;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        frontViper = hardwareMap.dcMotor.get("frontViper");
+        backViper = hardwareMap.dcMotor.get("backViper");
+        testEncoder = hardwareMap.dcMotor.get("testEncoder");
+
+        // Reset the encoder during initialization
+        frontViper.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backViper.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        testEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+        while (!isStopRequested() && opModeIsActive()) {
+
+            // Set the motor's target position to 300 ticks
+            testEncoder.setTargetPosition(2850);
+
+            // Switch to RUN_TO_POSITION mode
+            testEncoder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // Start the motor moving by setting the max velocity to 200 ticks per second
+            if (gamepad1.dpad_up) {
+                testEncoder.setPower(0.2);
+            }
+
+            // While the Op Mode is running, show the motor's status via telemetry
+//        while (opModeIsActive()) {
+//            telemetry.addData("power", testEncoder.getPower());
+//            telemetry.addData("position", testEncoder.getCurrentPosition());
+//            telemetry.addData("is at target", !testEncoder.isBusy());
+//            telemetry.update();
+//        }
+
+            // Loop while the motor is moving to the target
+            while ((testEncoder.isBusy()) && !isStopRequested()) {
+                // Let the drive team see that we're waiting on the motor
+                telemetry.addData("Status", "Waiting for the motor to reach its target");
+                telemetry.addData("power", testEncoder.getPower());
+                telemetry.addData("position", testEncoder.getCurrentPosition());
+                telemetry.addData("is at target", !testEncoder.isBusy());
+                telemetry.update();
+            }
+// The motor has reached its target position, and the program will continue
+
+            // Stop all motion;
+            testEncoder.setPower(0);
+
+            telemetry.addData("Status", "position achieved");
+            telemetry.update();
+
+            sleep(5000);   // optional pause after each move.
+        }
+    }
+}
