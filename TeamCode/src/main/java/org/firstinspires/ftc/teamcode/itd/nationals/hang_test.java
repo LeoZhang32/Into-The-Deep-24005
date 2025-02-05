22990010e64fee3cf5d29b71706a86df2a672c78
24005-admin <yang.zhang.2000@gmail.com>
2025-02-05 21:40:05 +0800

    edited hang test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itd/nationals/hang_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itd/nationals/hang_test.java
@@ -15,6 +15,12 @@ public class hang_test extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         v_slide_front = hardwareMap.get(DcMotor.class, "vslidefront");
         v_slide_back = hardwareMap.get(DcMotor.class, "vslideback");
+
+        v_slide_front.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        v_slide_back.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        v_slide_front.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        v_slide_back.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         waitForStart();
         if (isStopRequested()) return;
         while (!isStopRequested() && opModeIsActive()) {
@@ -26,6 +32,15 @@ public class hang_test extends LinearOpMode {
                 v_slide_front.setPower(-vert_power);
                 v_slide_back.setPower(vert_power);
             }
+
+
+            telemetry.addData("v slide front position", v_slide_front.getCurrentPosition());
+            telemetry.addData("v slide back position", v_slide_back.getCurrentPosition());
+            telemetry.addData("v slide front direction", v_slide_front.getDirection());
+            telemetry.addData("v slide back direction", v_slide_back.getDirection());
+            telemetry.update();
+
+
         }
     }
 }
