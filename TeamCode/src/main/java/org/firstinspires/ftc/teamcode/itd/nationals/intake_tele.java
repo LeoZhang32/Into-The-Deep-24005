8de58d406b55592f56d6c13e3d0cd8fa7ec17625
LeoZhang32 <leoxlzhang@outlook.com>
2025-02-06 15:23:37 +0800

    edited not working intake tele, still needs fixing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itd/nationals/intake_tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itd/nationals/intake_tele.java
@@ -18,8 +18,12 @@ public class intake_tele extends LinearOpMode {
     Servo IWrist;
     Servo IClaw;
     Servo IArmC;
+    Servo HSlideL;
+    Servo HSlideR;
 
     Servo OClaw;
+    Servo OArm;
+
 
     private final FtcDashboard dashboard = FtcDashboard.getInstance();
     @Override
@@ -30,13 +34,18 @@ public class intake_tele extends LinearOpMode {
 
         positions_and_variables pos = new positions_and_variables();
         CycleGamepad cycle_gamepad1 = new CycleGamepad(gamepad1);
+        CycleGamepad cycle_gamepad2 = new CycleGamepad(gamepad2);
+
         IArmL = hardwareMap.get(Servo.class, "IArmL");
         IArmR = hardwareMap.get(Servo.class, "IArmR");
         IArmC = hardwareMap.get(Servo.class, "IArmC");
         IWrist = hardwareMap.get(Servo.class, "IWrist");
         IClaw = hardwareMap.get(Servo.class, "IClaw");
+        HSlideL = hardwareMap.get(Servo.class, "HSlideL");
+        HSlideR = hardwareMap.get(Servo.class, "HSlideR");
 
         OClaw = hardwareMap.get(Servo.class, "OClaw");
+        OArm = hardwareMap.get(Servo.class, "OArm");
 
         OClaw.setPosition(pos.outtake_claw_open);
 
@@ -46,31 +55,46 @@ public class intake_tele extends LinearOpMode {
             cycle_gamepad1.updateX(4);
             cycle_gamepad1.updateRB(4);
             cycle_gamepad1.updateA(2);
-            cycle_gamepad1.updateB(2);
+
+            cycle_gamepad2.updateA(2);
+            cycle_gamepad2.updateX(2);
 
             dashboard.sendTelemetryPacket(packet);
 
             //arm movements
             if (cycle_gamepad1.xPressCount == 0){
+                HSlideL.setPosition(pos.hslide_trans);
+                HSlideR.setPosition(1-pos.hslide_trans);
                 IArmL.setPosition(pos.intake_arm_trans);
                 IArmR.setPosition(1-pos.intake_arm_trans);
                 IArmC.setPosition(pos.intake_coax_trans);
+                extendoIn = true;
             }
             else if (cycle_gamepad1.xPressCount == 1){
+                HSlideL.setPosition(pos.hslide_aim);
+                HSlideR.setPosition(1-pos.hslide_aim);
                 IArmL.setPosition(pos.intake_arm_aim);
                 IArmR.setPosition(1-pos.intake_arm_aim);
                 IArmC.setPosition(pos.intake_coax_aim);
+                extendoIn = false;
             }
             else if (cycle_gamepad1.xPressCount == 2){
+                HSlideL.setPosition(pos.hslide_aim);
+                HSlideR.setPosition(1-pos.hslide_aim);
                 IArmL.setPosition(pos.intake_arm_grab);
                 IArmR.setPosition(1-pos.intake_arm_grab);
                 IArmC.setPosition(pos.intake_coax_grab);
+                extendoIn = false;
             }
             else{
+                HSlideL.setPosition(pos.hslide_aim);
+                HSlideR.setPosition(1-pos.hslide_aim);
                 IArmL.setPosition(pos.intake_arm_lift);
                 IArmR.setPosition(1-pos.intake_arm_lift);
                 IArmC.setPosition(pos.intake_coax_lift);
+                extendoIn = false;
             }
+
             //wrist movements
             if (cycle_gamepad1.rbPressCount == 0){
                 IWrist.setPosition(pos.intake_wrist0);
@@ -84,6 +108,7 @@ public class intake_tele extends LinearOpMode {
             else{
                 IWrist.setPosition(pos.intake_wrist3);
             }
+
             //intake claw movement
             if (!extendoIn && !isTransferTimerRunning) {
                 if (cycle_gamepad1.aPressCount == 1) {
@@ -92,25 +117,38 @@ public class intake_tele extends LinearOpMode {
                     IClaw.setPosition(pos.intake_claw_open);
                 }
             }
+
             //outtake claw movement
-            if (cycle_gamepad1.bPressCount == 1) {
-                OClaw.setPosition(pos.outtake_claw_close);
-                IClaw.setPosition(pos.intake_claw_close);
+            if (cycle_gamepad2.aPressCount == 1) {
+                OClaw.setPosition(pos.outtake_claw_open);
+
                 transferTimer.reset();
                 isTransferTimerRunning = true;  // Indicate timer has started
+
             }
             else{
-                OClaw.setPosition(pos.outtake_claw_open);
+                OClaw.setPosition(pos.outtake_claw_close);
             }
 
             //Delayed IClaw opening
-            if (extendoIn && isTransferTimerRunning && transferTimer.milliseconds() >= 200) {
+            if (extendoIn && isTransferTimerRunning && transferTimer.milliseconds() >= 300) {
                     IClaw.setPosition(pos.intake_claw_open);
+//                    cycle_gamepad1.aPressCount = 0;
                     isTransferTimerRunning = false; // Stop tracking timer once done
             }
+            //outtake arm movement
+            if (cycle_gamepad2.xPressCount == 0){
+                OArm.setPosition(pos.outtake_arm_transfer);
+            }
+            else{
+                OArm.setPosition(pos.outtake_arm_sample);
+            }
 
-
-
+            telemetry.addData("Gamepad1 aPressCount", cycle_gamepad1.aPressCount);
+            telemetry.addData("Gamepad2 aPressCount", cycle_gamepad2.aPressCount);
+            telemetry.addData("Gamepad1 xPressCount", cycle_gamepad1.xPressCount);
+            telemetry.addData("Gamepad2 xPressCount", cycle_gamepad2.xPressCount);
+            telemetry.update();
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itd/nationals/positions_and_variables.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itd/nationals/positions_and_variables.java
@@ -3,13 +3,13 @@ package org.firstinspires.ftc.teamcode.itd.nationals;
 public class positions_and_variables {
     public double vert_slides_power = 1.0;
 
-    public double extendo_out = 0.5;
-    public double extendo_in = 0.5;
+    public double hslide_trans = 0.3;
+    public double hslide_aim = 0.2;
 
-    public double intake_arm_trans = 0.45;
-    public double intake_arm_aim = 0.5;
-    public double intake_arm_grab = 0.55;
-    public double intake_arm_lift = 0.6;
+    public double intake_arm_trans = 0.3;
+    public double intake_arm_aim = 0.6;
+    public double intake_arm_grab = 0.65;
+    public double intake_arm_lift = 0.4;
 
     public double intake_coax_trans = 0.45;
     public double intake_coax_aim = 0.5;
@@ -24,6 +24,10 @@ public class positions_and_variables {
     public double intake_wrist2 = 0.8;
     public double intake_wrist3 = 0.35;
 
-    public double outtake_claw_close = 0.5;
-    public double outtake_claw_open = 0.6;
+    public double outtake_claw_close = 0.4;
+    public double outtake_claw_open = 0.7;
+
+    public double outtake_arm_transfer = 0.5;
+    public double outtake_arm_sample = 0.4;
+    public double outtake_arm_specimen = 0.3;
 }
