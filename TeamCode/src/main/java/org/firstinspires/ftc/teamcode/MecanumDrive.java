3e577828eeaaa0761f3819a21c7f45462accb197
yangzhang2000 <yang.zhang.2000@gmail.com>
2025-11-27 00:05:26 +0800

    roadrunner tune 11/26 after adding weight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -64,13 +64,13 @@ public class MecanumDrive {
 
         // drive model parameters
         public double inPerTick = 1; // If you're using OTOS/Pinpoint leave this at 1 (all values will be in inches, 1 tick = 1 inch)
-        public double lateralInPerTick = 0.69855753004239693333333333333333; //avg lateralInPerTick 0.7545736812979547666667
-        public double trackWidthTicks = 14.71; //13.539911372758029
+        public double lateralInPerTick = 0.621077144461229; //TODO before adding weight: 0.69855753004239693333333333333333
+        public double trackWidthTicks = 13.299851701070400; //TODO before adding weight: 14.71 //13.539911372758029
 
         // feedforward parameters (in tick units) avg kV = 0.140391718664841026667 avg kS = 1.5253672305515611
-        public double kS = 1.2899544192594723;
-        public double kV = 0.13910233560284496;
-        public double kA = 0.03;
+        public double kS = 1.296037040606880; //TODO before adding weight: 1.2899544192594723
+        public double kV = 0.148440797813735; //TODO before adding weight: 0.13910233560284496
+        public double kA = 0.036; //TODO before adding weight: 0.03
 
         // path profile parameters (in inches)
         public double maxWheelVel = 70; //TODO previously chongqing 50
@@ -83,8 +83,8 @@ public class MecanumDrive {
 
         // path controller gains
         public double axialGain = 6;
-        public double lateralGain = 0.5;
-        public double headingGain = 2; // shared with turn
+        public double lateralGain = 0.4; //TODO before adding weight: 0.5
+        public double headingGain = 1.5; //TODO before adding weight: 2
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
